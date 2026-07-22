986aae26821ce97e35e79a2dc19d065b46f75b7a
gulloooooo2-crypto <gulloooooo2@gmail.com>
2026-07-22 22:50:22 +0545

    Update cloner.py

--- cloner.py
+++ cloner.py
@@ -180,8 +180,8 @@ while (loop == 'true'):
             password = raw_input("\033[1;97mðŸ—� \x1b[1;91mTool Password  \x1b[1;97m» \x1b[1;97m")
             if (password == 
     CorrectPassword):
-                         print "Logged in
-successfully as " + username #Dev:love_hacker
+                         print ("Logged in successfully as " 
+                         + username) #Dev:love_hacker
                     time.sleep(2)
                     loop = 'false'
             else:
